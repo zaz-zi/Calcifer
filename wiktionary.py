7631e8ff6f053e529971fffb05591a4cfbc4909b
savkorlev <evglev997@gmail.com>
2023-03-12 00:18:24 +0100

    Bold speech part

--- wiktionary.py
+++ wiktionary.py
@@ -11,8 +11,6 @@ import string
 
 def generateOutput(inputWord: str):
 
-    definitions = {}
-
     output = ''
 
     speechParts = [
@@ -41,8 +39,7 @@ def generateOutput(inputWord: str):
         elif len(currentTag.xpath(f'./span[@class="mw-headline"]')) > 0 and currentTag.xpath(f'./span[@class="mw-headline"]')[0].text in speechParts:
 
             speechPart = currentTag.xpath(f'./span[@class="mw-headline"]')[0].text
-            if speechPart not in definitions:
-                output = f'{output}{speechPart}\n\n'
+            output = f'{output}**{speechPart}**\n\n'
 
             if currentTag.xpath('following-sibling::*[1]')[0].tag == 'p':  # class="Latn headword", lang="en"
                 underSpeechPart = currentTag.xpath('following-sibling::*[1]')[0].text_content()
@@ -70,7 +67,6 @@ def generateOutput(inputWord: str):
             for j, li in enumerate(lis):
                 if j < 5:
                     output = f'{output}{j + 1}) {li}\n\n'
-            output = f'{output}\n'
 
     if output.replace('\n', '') == inputWord:
         output = 'Word or phrase not found'
