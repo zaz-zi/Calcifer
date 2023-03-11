61a88fc365d94f5d7b9b80cbca03e4b723eb105d
savkorlev <evglev997@gmail.com>
2023-03-11 20:21:59 +0100

    Added example usage

--- wiktionary.py
+++ wiktionary.py
@@ -20,7 +20,7 @@ def generateOutput(inputWord: str):
         'Adverb', 'Preposition', 'Postposition', 'Circumposition', 'Ambiposition',
         'Conjunction', 'Interjection', 'Exclamation', 'Particle', 'Clause', 'Proper noun',
         'Participle', 'Phrase'
-    ]
+        ]
 
     if inputWord[0] in string.ascii_letters:
         language = 'English'
@@ -31,39 +31,39 @@ def generateOutput(inputWord: str):
     tree = html.fromstring(url.content)
 
     for i in range(1, len(tree.xpath(f'//h2[span[@id="{language}"]]/following-sibling::*'))):
-        currentTag = tree.xpath(
-            f'//h2[span[@id="{language}"]]/following-sibling::*[{i}]')[0]  # why list
+        currentTag = tree.xpath(f'//h2[span[@id="{language}"]]/following-sibling::*[{i}]')[0]  # why list
         currentTagName = currentTag.tag
 
         if currentTagName == 'h2':
             break
 
         # elif currentTagName in ['h3', 'h4']:
-        elif len(currentTag.xpath(f'./span[@class="mw-headline"]')) != 0 and currentTag.xpath(f'./span[@class="mw-headline"]')[0].text in speechParts:
+        elif len(currentTag.xpath(f'./span[@class="mw-headline"]')) > 0 and currentTag.xpath(f'./span[@class="mw-headline"]')[0].text in speechParts:
 
-            speechPart = currentTag.xpath(
-                f'./span[@class="mw-headline"]')[0].text
+            speechPart = currentTag.xpath(f'./span[@class="mw-headline"]')[0].text
             if speechPart not in definitions:
                 output += speechPart + '\n\n'
 
-            # class="Latn headword", lang="en"
-            if currentTag.xpath('following-sibling::*[1]')[0].tag == 'p':
-                underSpeechPart = currentTag.xpath(
-                    'following-sibling::*[1]')[0].text_content()
+            if currentTag.xpath('following-sibling::*[1]')[0].tag == 'p':  # class="Latn headword", lang="en"
+                underSpeechPart = currentTag.xpath('following-sibling::*[1]')[0].text_content()
                 output += underSpeechPart + '\n'
 
-            ols = currentTag.xpath('./following-sibling::ol[1]/li')
+            lis = currentTag.xpath('./following-sibling::ol[1]/li')
 
             # extract the text from the tag and all its children
-            for j, ol in enumerate(ols):
-                ols[j] = ol.text_content().split('\n')[0].strip()
+            for j, li in enumerate(lis):
+                lis[j] = li.text_content().split('\n')[0].strip()
+                # usage example if exists
+                if len(li.xpath('.//*[@class = "h-usage-example"]')) > 0:
+                    for usageI in range(len(li.xpath('.//*[@class = "h-usage-example"]'))):
+                        lis[j] = lis[j] + '\n\t' + li.xpath('.//*[@class = "h-usage-example"]')[usageI].text_content()  # TODO: add a separator for the croner case мама
 
             # remove empty entries
-            ols = list(filter(None, ols))
+            lis = list(filter(None, lis))
 
-            for j, ol in enumerate(ols):
+            for j, li in enumerate(lis):
                 if j < 5:
-                    output = f'{output}{j + 1}) {ol}\n'
+                    output = f'{output}{j + 1}) {li}\n'
             output += '\n'
 
     if output.replace('\n', '') == inputWord:
