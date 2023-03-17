d0635b262d4caf740aa2503c4ac671e9b4b10044
zaz-zi <avvare2754@gmail.com@>
2023-03-17 21:09:33 +0300

    aboba

--- help.py
+++ help.py
@@ -20,14 +20,15 @@ async def can_do(interaction: discord.Interaction):
             botInfo = interaction.guild.get_channel(channels['bot-info'])
             botCommands = interaction.guild.get_channel(channels['bot-commands'])
         finalNoteEn = noteEn.replace('#bot-info', f"{botInfo.mention}").replace('#bot-commands', f"{botCommands.mention}")
-        finalNoteRu = noteEn.replace('#bot-info', f"{botInfo.mention}").replace('#bot-commands', f"{botCommands.mention}")
+        finalNoteRu = noteRu.replace('#bot-info', f"{botInfo.mention}").replace('#bot-commands', f"{botCommands.mention}")
+        finalString = f'{finalNoteEn}\n\n{finalNoteRu}'
         embedEn = discord.Embed(type='rich', title='What I can do:', description=enText, color=0xffa400)
         embedRu = discord.Embed(type='rich', title='Что я умею:', description=ruText, color=0xffa400)
         embedEn.set_author(name="Practice Your Russian & English", icon_url=interaction.guild.icon.url)
         embedRu.set_author(name="Practice Your Russian & English", icon_url=interaction.guild.icon.url)
         await interaction.response.send_message('Please stand by', ephemeral=True, delete_after=10)
-        await interaction.channel.send(finalNoteEn)
-        asyncio.sleep(60*10)
+        await interaction.channel.send(finalString)
+        await asyncio.sleep(60*10)
         await interaction.channel.send(embed=embedEn)
         await interaction.channel.send(embed=embedRu)
 
