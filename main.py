ccce56dbabb0d7de8aded7efffc61993b46691c2
zaz-zi <avvare2754@gmail.com@>
2023-03-23 22:14:30 +0300

    ylf

--- main.py
+++ main.py
@@ -129,10 +129,10 @@ async def on_message_delete(message):
                 async for entry in message.guild.audit_logs(limit=1, action=discord.AuditLogAction.message_delete):
                     if entry.target == message.author:
                         deleter = entry.user
-                        date = f'<t:{str(int(datetime.timestamp(message.created_at)))}:f>'
+                        date = message.created_at.strftime('%y.%m.%d | %H:%M')
                         await modLog.send(f'**{deleter.name}** deleted a message by **{message.author.name}** in {message.channel.mention}:\n({date}) {message.content}')
                     else:
-                        date = f'<t:{str(int(datetime.timestamp(message.created_at)))}:f>'
+                        date = message.created_at.strftime('%y.%m.%d | %H:%M')
                         await modLog.send(f'Deleted message by **{message.author.name}** in {message.channel.mention}:\n({date}) **{message.content}**')
                 
                 
