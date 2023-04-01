c0d87898b3320b56bc7b998734cdfde0fdb5aba6
zaz-zi <avvare2754@gmail.com@>
2023-04-01 19:05:42 +0300

    xdftgnfhm

--- translate.py
+++ translate.py
@@ -16,13 +16,32 @@ async def translate(interaction: discord.Interaction, target_lang: str, phrase: 
             target_lang += ' (american)'
         if source_lang == 'english':
             source_lang += ' (american)'
+        if target_lang == 'portuguese':
+            target_lang += ' (brazilian)'
+        if source_lang == 'portuguese':
+            source_lang += ' (brazilian)'
+        if target_lang == 'en':
+            target_lang = 'en-us'
+        if source_lang == 'en':
+            source_lang = 'en-us'
+        if target_lang == 'pt':
+            target_lang = 'pt-br'
+        if source_lang == 'pt':
+            source_lang = 'pt-br'
+        if target_lang == 'norwegian':
+            target_lang += ' (bokmal)'
+        if source_lang == 'norwegian':
+            source_lang += ' (bokmal)'
+        if target_lang == 'chinese':
+            target_lang += ' (simplified)'
+        if source_lang == 'chinese':
+            source_lang += ' (simplified)'
         for item in langs.keys():
             if target_lang == langs[item].lower():
                 target_lang = item
             if source_lang == langs[item].lower():
                 source_lang = item
-        if target_lang == 'en':
-            target_lang = 'en-us'
+        
         if source_lang == 'auto':
             if source_lang == 'en':
                 source_lang = 'en-us'
